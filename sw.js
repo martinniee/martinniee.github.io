ce0a1185cada36fec30879cf269158cb028a604f
martinniee <1954924950@qq.com>
2023-02-22 19:54:17 +0800

    Site updated: 2023-02-22 19:54:16

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","36aa7d3fc457f0139031acd51cf5606e"]];
+var precacheConfig = [["/sw-register.js","6cc5787414551533c267fe1f550b2de2"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
